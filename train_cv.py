d54cc6f3503a1397b4c612f5c5273b575f4954e4
Cicilia Lee <aacs0130@gmail.com>
2016-03-17 04:56:39 -0500

    Change train svc program to load test data every 10K records

--- train_cv.py
+++ train_cv.py
@@ -7,19 +7,20 @@ from sklearn.grid_search import GridSearchCV
 
 if __name__ == "__main__":
     logging.basicConfig(format='--%(asctime)s:[%(levelname)s]:%(lineno)d:%(message)s', datefmt='%Y/%m/%d %H:%M:%S', level=logging.INFO)
-    logging.info("avazu_train.py Start")
+    logging.info("Program Start")
     p = PreProcess()
-    PREPROCESS = False
+    #PREPROCESS = False
 
-    filepath = 'data/train_1M.csv'
+    #filepath = 'data/train_1M.csv'
     #out_filepath = 'data/train_1M.csv.out'
-    out_filepath = 'data/train_s404_1K.out'
+    out_filepath = 'data/train_s404_10K.out'
     #out_filepath = 'data/train_1000.csv.out'
-    if PREPROCESS:
-        out_filepath = p.convert(filepath)
+    #if PREPROCESS:
+    #    out_filepath = p.convert(filepath)
 
     #Load data
-    X, y = p.load_train_data(out_filepath)
+    X, y, enc, map_dict = p.load_train_data(out_filepath, category = True)
+    #X, y = p.load_train_data(out_filepath)
     logging.info("Shape X = %r, y =%r" %(X.shape, y.shape ))
     logging.info("example X = %s\ny =%r" %(X[0], y[0]))
     logging.info("classes: %r" % list(np.unique(y)))
@@ -43,7 +44,7 @@ if __name__ == "__main__":
     logging.info("Samples : %d, CV :%d" % (n_subsamples, CV))
     
     #Kernal
-    KERNEL = 'linear'
+    KERNEL = 'rbf'
     #CV
     svc_params = {
             'C': np.logspace(-1, 2, 4),
@@ -69,26 +70,33 @@ if __name__ == "__main__":
     for part in range(1, 6):
         test_filepath = test_filepattern % part
         logging.info("Loading test set [%s]..." % test_filepath)
-        X_test, ids_test= p.load_test_data(test_filepath)
-        logging.info("Shape X = %r, ids =%r" %(X_test.shape, ids_test.shape ))
-        logging.info("example X = %s\nids =%r" %(X_test[0], ids_test[0]))
-        svc_probs = gs_svc.predict_proba(X_test)
-        #[prob of 0, prob of 1]
-        logging.info("prob of test: %s" % svc_probs[:10])
-        
-        out_filepath = "%s-svc-1vs1-s%d-c%f-g%f.csv" %(test_filepath, n_subsamples, gs_svc.best_params_['C'], gs_svc.best_params_['gamma'])
-        logging.info("Writing out file %s" % out_filepath)
-        if len(ids_test) != len(svc_probs):
-            logging.error("Test case count don:t match")
-        else :
-            with open(out_filepath, 'a') as ofile:
-                writer = csv.DictWriter(ofile, field)
-                if part == 1:
-                    writer.writeheader()
-                for i in range(len(ids_test)):
-                    row = {'id' : ids_test[i], 'click' : svc_probs[i][1]}
-                    #logging.info("row %d : %s" %(i, row))
-                    writer.writerow(row)
+        #X_test, ids_test= p.load_test_data(test_filepath)
+        #Load data with category
+
+        width = 10000
+        for a_slice in range(100):
+            logging.info("+++Doing slice %d+++" %(a_slice))
+            X_test, ids_test= p.load_test_data(test_filepath, enc = enc, map_dict = map_dict, start_line_no = a_slice*width)
+            if X_test == None:
+                break
+            logging.info("Shape X = %r, ids =%r" %(X_test.shape, ids_test.shape ))
+            logging.info("example X = %s\nids =%r" %(X_test[0], ids_test[0]))
+            svc_probs = gs_svc.predict_proba(X_test)
+            #[prob of 0, prob of 1]
+            logging.info("prob of test: %s" % svc_probs[:10])
+            out_filepath = "%s-svc-s%d-c%f-g%f.csv" %(test_filepath, n_subsamples, gs_svc.best_params_['C'], gs_svc.best_params_['gamma'])
+            logging.info("Writing out file %s" % out_filepath)
+            if len(ids_test) != len(svc_probs):
+                logging.error("Test case count don:t match")
+            else :
+                with open(out_filepath, 'a') as ofile:
+                    writer = csv.DictWriter(ofile, field)
+                    if part == 1:
+                        writer.writeheader()
+                    for i in range(len(ids_test)):
+                        row = {'id' : ids_test[i], 'click' : svc_probs[i][1]}
+                        #logging.info("row %d : %s" %(i, row))
+                        writer.writerow(row)
 
     
-    logging.info("avazu_train.py End")
+    logging.info("Program End")
